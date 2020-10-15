4624f04967f7aee3162e751bff69f58dc3ded3f5
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-10-16 00:08:28 +0300

    Define Playlist widget

---
+++ lib/ui/screens/playlist/widgets/playlist_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class PlaylistWidget extends StatelessWidget {
+  final String coverUrl,albumTitle,singerName;
+
+  PlaylistWidget({this.coverUrl,this.albumTitle,this.singerName});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        InkWell(
+          onTap: (){},
+          child: Container(
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(4.0),
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    child: Image.network(coverUrl),
+                  ),
+                ),
+                SizedBox(width: 20.0,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      albumTitle,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.w800
+                      ),
+                    ),
+                    SizedBox(height: 12.0,),
+                    Text(
+                      singerName,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w300
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 20.0,)
+      ],
+    );
+  }
+}

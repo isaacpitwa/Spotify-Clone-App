50324cb8a81ab1df28d5bf89bdf1485fb6ca0c36
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-10-15 23:42:28 +0300

    Home Content scren

---
+++ lib/ui/screens/homeContent/home_content.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeContent extends StatelessWidget {
+  List<String> CoverList =[
+  "https://i.pinimg.com/originals/f5/82/47/f58247463e38a536f442bfb816f62c6b.jpg",
+  "https://www.designformusic.com/wp-content/uploads/2016/09/electro-synthwave-album-cover-500x500.jpg",
+    "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/102342461/original/68ef1e1fab3c4028d51f7fd7cfa9bad2232e576c/create-a-copyright-free-album-cover.jpg",
+    "https://www.designformusic.com/wp-content/uploads/2016/02/volta-music-trailer-music-album-cover-design.jpg",
+    "https://www.designformusic.com/wp-content/uploads/2018/07/Digital-World-album-cover-design.jpg",
+    "https://www.designformusic.com/wp-content/uploads/2016/02/volta-music-trailer-music-album-cover-design.jpg"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

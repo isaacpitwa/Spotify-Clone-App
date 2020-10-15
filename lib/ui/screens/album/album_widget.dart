b9e0529ac14d8d1236c971be29120111d5e074dc
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-10-15 23:45:47 +0300

    Album widget COntent

--- lib/ui/screens/album/album_widget.dart
+++ lib/ui/screens/album/album_widget.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
 class AlbumWidget extends StatelessWidget {
+  final  String coverUrl,coverName,singerName;
+  AlbumWidget({this.coverUrl,this.coverName,this.singerName});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: InkWell(
+        onTap: (){},
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Container(
+                  height: 140.0,
+                  width: 140.0,
+                  child: Image.network(coverUrl)
+              ),
+            ),
+            SizedBox(height: 12.0,),
+            Text(
+              coverName,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 22.0
+              ),
+            ),
+            SizedBox(height: 12.0,),
+            Text(
+              singerName,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 18.0
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
